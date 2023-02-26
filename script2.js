c6e889589924c4d8c9f2ccaa0cc544b92e2cafbc
AnnLP2 <ann.vitalina@gmail.com>
2023-02-26 22:15:56 +0200

    created homework, part 2

---
+++ script2.js
@@ -0,0 +1,9 @@
+let a = Number(prompt ("First number"));
+let b = Number(prompt ("Second number"));
+function calculate (a, b) {
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a * b);
+    console.log(a / b);
+}
+calculate(a, b);

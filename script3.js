e3b22c0a818e0a5096a0d8a6fb6689b7ea4638de
AnnLP2 <ann.vitalina@gmail.com>
2023-02-27 01:15:19 +0200

    created homework, part 3

--- script3.js
+++ script3.js
@@ -1,1 +1,14 @@
-let a;
+// Task #1
+let admin;
+let name = "John";
+admin = name;
+alert(admin);
+
+// Task #2
+let planetName = "Earth";
+let currentUserName = "Ann";
+
+
+// Task #3
+let a = '10' + 10;
+console.log(typeof a);

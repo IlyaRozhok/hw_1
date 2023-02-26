f261296699b5257cdc5c8562de4a1a743d32fb10
AnnLP2 <ann.vitalina@gmail.com>
2023-02-26 23:33:50 +0200

    finished calculatior

--- script2.js
+++ script2.js
@@ -1,9 +1,10 @@
 let a = Number(prompt ("First number"));
 let b = Number(prompt ("Second number"));
 function calculate (a, b) {
-    console.log(a + b);
-    console.log(a - b);
-    console.log(a * b);
-    console.log(a / b);
+    console.log("Result of addition is " + (a + b));
+    console.log("Result of subtraction is " + (a - b));
+    console.log("Result of multiplication is " + (a * b));
+    console.log("Result of division is " + (a / b));
 }
+
 calculate(a, b);

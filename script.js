b6ab76e6728cb2195dc54b878863fa6630a3d53b
AnnLP2 <ann.vitalina@gmail.com>
2023-02-26 17:55:47 +0200

    create homework, part 1

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 let userName = prompt("What is your name?");
 // alert("Hello, " + userName + "! How are you?");
 alert(`Hello, ${userName}! How are you?`);
+
+
+// if (userName === null) {
+//     alert("Ok, bye!");
+// }
+//     else if (userName === '') {
+//         alert("Please, answer again");
+//
+// } else {
+//     alert(`Hello, ${userName}! How are you?`);
+// }
